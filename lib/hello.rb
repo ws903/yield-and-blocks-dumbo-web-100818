97e6d8f1b4251ee76d571247203eeab68da4e956
David Sim <wsim903@gmail.com>
2018-09-26 21:03:13 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,15 @@
-def hello_t
-
+def hello_t(arr)
+  if block_given?
+    arr.each do |name| 
+      if name.end_with?("m")
+        puts name
+      else
+        puts name.upcase
+      end
+    end
+  else
+    puts "Hey! No block was given!"
+  end
 end
 
-# call your method here!
 
