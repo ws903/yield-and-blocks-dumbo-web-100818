32feef2c143962f6e76f1fe3b9f835ceaed5e41f
David Sim <wsim903@gmail.com>
2018-09-26 21:01:05 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -2,12 +2,10 @@ def hello_t(arr)
   if block_given?
     arr.each do |name| 
       if name.end_with?("m")
-        puts
+        puts name
       else
-       puts name.upcase
+        puts name.upcase
       end
-      puts out
-    end
   else
     puts "Hey! No block was given!"
   end
