a00c33696aaedeaa3c54ea37947bd13bafdd1c8a
David Sim <wsim903@gmail.com>
2018-09-26 20:54:07 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,7 +1,7 @@
 def hello_t(arr)
   i = 0
-  while i < array.length  
-    if array[i].end_with?("m")
+  while i < arr.length  
+    if arr[i].end_with?("m")
       yield(name)
     else
       yield(ame.upcase)
